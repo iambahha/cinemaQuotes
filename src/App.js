c6b1e655273faa4af81058b3df02fc64e9368eb0
Baktiyar Ishenov <b1963826@gmail.com>
2020-04-03 21:08:32 +0600

    fixed bag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch,} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Navigation from "./Components/UI/Navigation/Navigation";
 import AddQuote from "./Containers/AddQuote/AddQuote";
 import QuotesList from "./Containers/QuotesList/QuotesList";
@@ -12,7 +12,7 @@ function App() {
           <Navigation />
           <div className="container mt-5">
               <Switch >
-                  <Route path="/cinemaQuotes" exact component={QuotesList}/>
+                  <Route path="/cinemaQuotes/" exact component={QuotesList}/>
                   <Route path="/cinemaQuotes/quotes/add" component={AddQuote}/>
                   <Route path="/cinemaQuotes/quotes-edit/:id" component={EditQuote}/>
                   <Route path="/cinemaQuotes/quotes/:category" component={QuotesList}/>
