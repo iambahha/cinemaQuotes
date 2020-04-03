ef1d33738a0c4a505e5a6da71b8f72864d06416b
Baktiyar Ishenov <b1963826@gmail.com>
2020-04-03 20:58:49 +0600

    Updates

---
+++ precache-manifest.7e2a5c4aa281562a0cb5c0ef905b0a4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546b3b28756caedb5265412e13319882",
+    "url": "/cinemaQuotes/index.html"
+  },
+  {
+    "revision": "ee943f5f3c3638dae970",
+    "url": "/cinemaQuotes/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0214d97e8777bde47df2",
+    "url": "/cinemaQuotes/static/css/main.b0ac08ec.chunk.css"
+  },
+  {
+    "revision": "ee943f5f3c3638dae970",
+    "url": "/cinemaQuotes/static/js/2.2a448081.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/cinemaQuotes/static/js/2.2a448081.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0214d97e8777bde47df2",
+    "url": "/cinemaQuotes/static/js/main.52c0ff25.chunk.js"
+  },
+  {
+    "revision": "810cba359bc7f7f258f9",
+    "url": "/cinemaQuotes/static/js/runtime-main.4d4ef68e.js"
+  }
+]);
