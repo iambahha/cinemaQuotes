392dc5d9e9cadf0689800f0aef180a98ab4582c0
Baktiyar Ishenov <b1963826@gmail.com>
2020-04-03 21:12:08 +0600

    fixed bag

--- src/Components/UI/Navigation/Navigation.js
+++ src/Components/UI/Navigation/Navigation.js
@@ -7,10 +7,10 @@ const Navigation = () => {
             <div className="navbar-brand container">
                 <ul className="navbar-nav">
                     <li className="nav-item">
-                        <NavLink exact className="nav-link" to="/">Quotes<span className="sr-only">(current)</span></NavLink>
+                        <NavLink exact className="nav-link" to="/cinemaQuotes/">Quotes<span className="sr-only">(current)</span></NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link" to="/quotes/add">Submit New Quote</NavLink>
+                        <NavLink className="nav-link" to="/cinemaQuotes/quotes/add">Submit New Quote</NavLink>
                     </li>
                 </ul>
             </div>
